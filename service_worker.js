f572f09cc1298e9254ee3cd451ee3277f409d14d
Иagi <33362712+nagiept@users.noreply.github.com>
2020-05-06 14:46:25 +0900

    Add files via upload

---
+++ service_worker.js
@@ -0,0 +1,27 @@
+// cache file settings
+var CACHE_NAME = 'pwa-sample-caches';
+var urlsToCache = [
+	'/tlmaker.github.io/',
+];
+
+// installation
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches
+			.open(CACHE_NAME)
+			.then(function(cache) {
+				return cache.addAll(urlsToCache);
+			})
+	);
+});
+
+// cache loding
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then(function(response) {
+				return response ? response : fetch(event.request);
+			})
+	);
+});
